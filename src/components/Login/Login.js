09a4022cec2a3844a2296cf7c623fbad0fb77e58
Elijah Mbugi <mukundielijah@gmail.com>
2022-06-29 11:40:27 +0300

    error handling in signup form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Login.css'
 import {auth} from '../../components/Fireconfig';
 import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth';
+import {useNavigate} from 'react-router-dom';
 
 function Login() {
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const [error, setError] = React.useState('');
-    const [hasAccount, setHasAccount] = React.useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [hasAccount, setHasAccount] = useState(false);
 
-    const handleSignup = async () =>{
+    // const navigate = useNavigate();
+
+    const handleSignup = async (e) =>{
+        e.preventDefault();
         try{
-            await createUserWithEmailAndPassword(auth, email, password);
-            setHasAccount(true);
+            const user = await createUserWithEmailAndPassword(auth, email, password);
+            // navigate('/Dashboard');
         }catch(error){
-            setError(error.message);
+            setError(error.code==='auth/email-already-in-use'? 'Email already in use': error.message);
+            console.log(error.message);
         }
+ 
     }
-    const handleLogin = async () =>{
+    
+    const handleLogin = async (e) =>{
+        e.preventDefault();
         try{
-            await signInWithEmailAndPassword(auth, email, password);
-            setHasAccount(true);
+            const user = await signInWithEmailAndPassword(auth, email, password);
+            // navigate('/Dashboard');
         }catch(error){
             setError(error.message);
         }
@@ -34,24 +43,36 @@ function Login() {
             <label>Username:
             <i className="material-icons left">person</i>
             </label>
-            <input type="text" name="username" 
+            <input type="text" name="username"
+            value={name} 
+            onChange={(e) => setName(e.target.value)}
             />
             <label>Email:
             <i className="material-icons left">email</i>
             </label>
-            <input type="email" name="email" />
+            <input type="email" name="email" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            />
             <label>Password:
             <i className="material-icons left">password</i>
             </label>
-            <input type="password" name="password" />
+            <input type="password" name="password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            />
             
             <div className="classbtn">
+            <div className="error_div">
+          <p className="error">{error}</p>
+          
+        </div>
         {hasAccount ? (
           <>
             <button onClick={handleLogin}>Sign in</button>
             <p id="sign">
               Don't have an account ?{" "}
-              <span onClick={() => setHasAccount(!hasAccount)}>Sign Up</span>
+              <span onClick={() => setHasAccount(!hasAccount)}className="sign-link" >Sign Up</span>
             </p>
           </>
         ) : (
@@ -59,7 +80,7 @@ function Login() {
             <button onClick={handleSignup}>Sign up</button>
             <p>
               Have an account ?{" "}
-              <span onClick={() => setHasAccount(!hasAccount)}>Sign in</span>
+              <span onClick={() => setHasAccount(!hasAccount)} className="sign-link">Sign in</span>
             </p>
           </>
         )}
